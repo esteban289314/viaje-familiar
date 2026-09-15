eb110855c7a0baef1507fd56498bf5c06c8df6cf
esteban289314 <obandosarmiento@gmail.com>
2026-09-14 22:03:43 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='viaje-v5-0-10-oficial';
+const CACHE='viaje-v6-0-oficial';
 const CORE=['./','./index.html','./app.css','./app.js','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('viaje-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
